0f415b6250fd7d5cd9eed17db9e8685f7f45bc80
Roger Steve Ruiz <hi@rog.gr>
2024-12-17 16:09:18 -0500

    Añadiendo los guiones;
    Estos guiones los copier de este enlace: https://joshspicer.com/wireguard-azure

---
+++ guiones/variables.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Nuestro Hueco
+# Copyright (C) 2024 Roger Steve Ruiz
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
+# El nombre de los recursos que vamos crear para usar Pi-Hole, PiVPN, y
+# WireGuard. En español lo vamos a llamar [p]rotector de [c]ables usando
+# [a]gujero [p]i. Esto lo puedos cambiar a cualquer nombre que queras.
+export NOMBRE='pcap'
+
+# La ubicación pa' los recuros que vamos a crear.
+export UBICACION='northcentralus'
